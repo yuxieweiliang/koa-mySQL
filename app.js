f5b56a5564898565a6638c53c6dd98b4f818f98d
风吹雪 <yunruofengsheng@126.com>
2018-05-15 16:19:37 +0800

    'chagne'

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 var http = require('http');
 
 var mysql = require('mysql');
-var TEST_DATABASE = 'test';
+var TEST_DATABASE = '';
 var TEST_TABLE = 'test';
 var TEST2_TABLE = 'test2';
 
@@ -25,27 +25,45 @@ connection.connect(function(err) {
 });
 
 //创建数据库
-connection.query('CREATE DATABASE '+TEST_DATABASE, function(err) {
+/*connection.query('CREATE DATABASE test1', function(err) {
   if (err && err.number != mysql.ERROR_DB_CREATE_EXISTS) {
     throw err;
   }
-});
+});*/
+
+//删除数据库
+/*connection.query('DROP DATABASE test1', function(err) {
+  if (err && err.number != mysql.ERROR_DB_CREATE_EXISTS) {
+    throw err;
+  }
+});*/
 
 //不指定回调函数，如果出错，则体现为客户端错误
-connection.query('USE '+TEST_DATABASE, function(err) {
+connection.query('USE test', function(err) {
   if (err && err.number != mysql.ERROR_DB_CREATE_EXISTS) {
     throw err;
   }
 });
 
+const createTeacher = `
+  CREATE TABLE teacher(
+    id INT(11) AUTO_INCREMENT,
+    name VARCHAR(255),
+    age VARCHAR(255),
+    PRIMARY KEY (id)
+  )
+`
+
 //创建表格,插入数据
-/*connection.query(
-  'CREATE TABLE '+TEST2_TABLE+
-  '(id INT(11) AUTO_INCREMENT, '+
-  'name VARCHAR(255), '+
-  'age VARCHAR(255), '+
-  'PRIMARY KEY (id))'  // 约束主键id必须有且唯一
-);*/
+connection.query(createTeacher, function(err, results, fields) {
+    if(err) {
+      
+    } else {
+      
+    }
+
+    console.log(err)
+  });
 
 /*connection.query(
   'INSERT INTO '+TEST2_TABLE+' '+
@@ -61,7 +79,7 @@ var query = connection.query(
 
 //查询，并设置回调函数
 connection.query(
-  'SELECT * FROM '+ TEST2_TABLE,
+  'SELECT * FROM test',
   function selectCb(err, results, fields) {
     if (err) {
       throw err;
